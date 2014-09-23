c96b44db61681b18341011e68586698204fcb3db
Jonathan Wrobel <jwrobes@gmail.com>
2014-09-23 12:58:07 -0700

    Add foursquare api service and javascript for getting location data

---
+++ app/assets/javascripts/get_GeoLocation.js
@@ -0,0 +1,28 @@
+// class @GeoLocation
+	
+
+// 	getMe: ->
+// 		return true
+
+var geoLocation = {
+	what: function(){
+		return false;
+	},
+	create: function(){
+		var newHand = Object.create(this);
+		return newHand;
+	}	
+}
+
+var testMe = {
+	dude: function (){
+		return true;
+	},
+	getLocation: function () {
+		navigator.geolocation.getCurrentPosition(this.setGeoCookie);
+	},
+	setGeoCookie: function (position) {
+	var cookie_val = position.coords.latitude + "," + position.coords.longitude;
+  document.cookie = "lat_lng=" + escape(cookie_val);
+	}
+}
