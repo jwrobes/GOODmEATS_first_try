ad34df978094c18883f7a72bea966fc3148ceaeb
Jonathan Wrobel <jwrobes@gmail.com>
2014-09-23 12:54:02 -0700

    Add link to add new restaurant path and model for new_restaurants tthat will not be in database

---
+++ app/models/new_restaurant.rb
@@ -0,0 +1,10 @@
+class New_Restaurant
+
+	attr_reader :name, :location
+
+	def initialize(name, location)
+		@name = name
+		@location = location
+	end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920051839) do
+ActiveRecord::Schema.define(version: 20140920184152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: true do |t|
+    t.string "name"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
