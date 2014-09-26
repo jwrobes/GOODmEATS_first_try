b3e9eedd46fafd19553f5a7ab8a536af14622ca0
Jonathan Wrobel <jwrobes@gmail.com>
2014-09-26 10:58:21 -0700

    add cookie stub to add new restaurant test and create test for adding new restuarnt into database

--- spec/features/restaurants/restaurant_spec.rb
+++ spec/features/restaurants/restaurant_spec.rb
@@ -1,27 +1,35 @@
 require 'spec_helper'
 
 feature 'User signed in to website can add new restaurant' do
-	context "on home page can navigate to add new restaurant page" do
-		it "can navigate to add new restaurant page" do
-			visit root_path
-			click_on "Add Restaurant"
-			expect(page).to have_text('Find Restaurant')
-		end
-	end
+	# context "on home page can navigate to add new restaurant page" do
+	# 	it "can navigate to add new restaurant page" do
+	# 		visit root_path
+	# 		click_on "Add Restaurant"
+	# 		expect(page).to have_text('Find Restaurant')
+	# 	end
+	# end
 	
 
-	context "on add new restaurant page", js: true do
-		it "can enter restaurant name in search bar and get list of matching restaurants" do
-				visit add_restaurants_path
-				sleep(3)
-				fill_in 'search', with: 'parkway burger'
-				click_on "Search"
-				sleep(8)
-				expect(page).to have_text('park burger')
-		end
-		# it "can enter click add restaurant to database and get form to enter details on restaurant and add restaurant to database" do
-		
+	context "on add new restaurant page" do
+
+		# it "can enter restaurant name in search bar and get list of matching restaurants" do
+		# 		visit add_restaurants_path
+		# 		fill_in 'search', with: 'parkway burger'
+		# 		create_cookie('lat_lng','37.8021100|-122.2491410')
+				
+		# 		click_on "Search"
+			
+		# 		expect(page).to have_text('park burger')
 		# end
+
+		it "can click on add restaurant and go to form to enter new good meats restaurant in database", js: true do
+			visit add_restaurants_path
+			fill_in 'search', with: 'parkway burger'
+			create_cookie('lat_lng','37.8021100|-122')
+			click_on "Search"
+			first(:link, 'Select Restaurant').click
+			expect(page).to have_text('Burger King')
+		end
 	end
 
 end
