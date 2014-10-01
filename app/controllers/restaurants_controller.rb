fe2feb60cbe7a33897bf14499a4bd2c62658e699
Jonathan Wrobel <jwrobes@gmail.com>
2014-10-01 12:13:59 -0700

    added test to add new meat source to new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,8 +2,10 @@ class RestaurantsController < ApplicationController
 
 	def new
 		if Restaurant.find_by_foursquare_id(params["foursqaure_id"])
+			binding.pry
 			@restaurant = Restaurant.find_by_foursquare_id(params["foursqaure_id"])
 		else
+			binding.pry
 			@restaurant = Restaurant.new({name: params["name"], location: params["location"], foursquare_id: params["foursquare_id"], latitude: params[:location]["lat"], longitude: params[:location]["longitude"]})
 		end
 	end
